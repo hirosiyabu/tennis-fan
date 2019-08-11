e6e8f5cbccf0bf1fbca262d18e1aa4b6b06fd833
yabu <yh72yh72@gmail.com>
2019-08-11 16:40:44 +0900

    マイグレーションエラー対策

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // herokuマイグレーション用
+        Schema::defaultStringLength(191);
+
         // 本番環境(Heroku)でhttpsを強制する
         if (\App::environment('production')) {
             \URL::forceScheme('https');
