2b6435062b22725b91bc058deccd4746e99cfe38
yabu <yh72yh72@gmail.com>
2019-08-18 17:01:25 +0900

    マイグレーション設定

--- app/Http/Controllers/Auth/SocialLiteController.php
+++ app/Http/Controllers/Auth/SocialLiteController.php
@@ -19,7 +19,29 @@ class SocialLiteController extends Controller
 
     public function callback($social)
     {
-        $user = Socialite::driver('facebook')->user();
-        dd($user);
+        //ソーシャルサービス（情報）を取得
+        $userSocial = Socialite::driver($social)->stateless()->user();
+        //emailで登録を調べる
+        $user = User::where(['email' => $userSocial->getId()])->first();
+
+        //登録（email）の有無で分岐
+        if($user){
+
+            //登録あればそのままログイン（2回目以降）
+            Auth::login($user);
+            return redirect('/post');
+
+        }else{
+
+            //なければ登録（初回）
+            $newuser = new User;
+            $newuser->name = $userSocial->getName();
+            $newuser->email = $userSocial->getId();
+            $newuser->save();
+
+            //そのままログイン
+            Auth::login($newuser);
+            return redirect('/post');
+        }
     }
 }
