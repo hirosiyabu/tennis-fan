4ef0e3dad23e515e0be8b38639239346553f124d
yabu <yh72yh72@gmail.com>
2019-09-16 19:41:11 +0900

    微調整

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -15,12 +15,13 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
 
-        return $next($request);
-    }
+    
+        public function boot()
+        {
+          if ($this->app->environment() == 'production') {
+               URL::forceScheme('https');
+           }
+        }
+    
 }
