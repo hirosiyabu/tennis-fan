1ede102f12030b62640eb1d78be6d090d568e7f7
yabu <yh72yh72@gmail.com>
2019-09-21 19:47:23 +0900

    httpsに来た時に修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,8 +28,8 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         // 本番環境(Heroku)でhttpsを強制する
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+        if (\App::environment('heroku')) {
+            \URL::forceScheme('http');
         }
     }
 }
